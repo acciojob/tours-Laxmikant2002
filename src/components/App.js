8a4f0e192aed73587a6cef5f6f3820368fc6ca1c
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-11-24 13:36:42 +0530

    task

--- src/components/App.js
+++ src/components/App.js
@@ -54,7 +54,7 @@ const App = () => {
     return (
       <main>
         <div className="title">
-          <h2>No Tours Left</h2>
+          <h2>No tours left</h2>
           <button className="btn" onClick={fetchTours}>
             Refresh
           </button>
